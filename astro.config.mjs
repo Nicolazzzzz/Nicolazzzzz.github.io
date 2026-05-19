387a8c65c63e2ebd503a9b68e234bb7dddacf6f5
k0dr4 <nicolaszambranoabril12@gmail.com>
2026-05-19 01:36:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon';
-
 export default defineConfig({
   site: 'https://Nicolazzzzz.github.io',
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  vite: { plugins: [tailwindcss()] },
   integrations: [icon()]
 });
